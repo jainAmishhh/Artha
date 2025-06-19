824e44e46a3391389d2d3a53f05fdc23acf04699
jainAmishhh <jainamish1511@gmail.com>
2025-06-20 00:47:36 +0530

    modified the layout.jsx file

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header/Header'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const Layout = () => {
   return (
-    <div>
-        <Header />
+    <>
+      <Header />
+      <main className="px-4 py-6">
         <Outlet />
-    </div>
-  )
-}
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
